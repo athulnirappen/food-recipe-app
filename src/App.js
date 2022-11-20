83aee4e9f94c5ed71cf38d869206e8411d18a2be
Athul Thomas <athulthomas001798@gmail.com>
2022-11-20 11:31:26 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+
+import "./Components/style.css"
+import Meal from './Components/Meal';
+import { Routes, Route } from "react-router-dom";
+import Recipieinfo from "./Components/Recipieinfo";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Routes>
+        <Route path="/" element={<Meal />} />
+        <Route path="/:MealId" element={<Recipieinfo/>}/>
+      </Routes>
+
+
     </div>
   );
 }
---
+++ src/Components/Meal.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import Mealitem from './Mealitem';
+import Mealrecipeindex from './Mealrecipeindex';
+
+
+const Meal = () => {
+  const[url,setUrl]=useState("https:/www.themealdb.com/api/json/v1/1/search.php?f=a");
+  const[item,setItem]=useState()
+  const[show,setshow]=useState(false)
+  const[search,setSearch]=useState("");
+  useEffect(()=>{
+    fetch(url).then(res=>res.json()).then(data=>{
+      console.log(data.meals);
+      setItem(data.meals)
+      setshow(true);
+    })
+
+  },[url])
+
+  const setIndex=(alpha)=>{
+    setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`)
+
+  }
+
+
+  const searchRecipie=(event)=>{
+    if(event.key==="Enter"){
+      setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
+    }
+
+  }
+
+
+
+
+  return (
+    <div className='main'>
+        <div className='heading'>
+            <h1>Search Your Food Recipe </h1>
+            <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor </h4>
+        </div>
+        <div className='searchbox'>
+            <input type="search" className='searchbar' onChange={(e)=>{
+              setSearch(e.target.value)
+            }} onKeyPress={searchRecipie}/>
+        </div>
+        <div className='container'>
+          {
+            show ? <Mealitem data={item}/> :"Not found"
+          }
+            
+        </div>
+        <div className='indexcontainer'>
+          <Mealrecipeindex alphaIndex={(alpha)=>{
+            setIndex(alpha)
+          }}/>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Meal;
+
