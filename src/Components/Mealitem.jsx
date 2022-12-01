16cf08efea210a9bb94815a62016480f6e1c0a1a
Athul Thomas <athulthomas001798@gmail.com>
2022-11-30 21:42:27 -0800

    mealitem is set

--- src/Components/Mealitem.jsx
+++ src/Components/Mealitem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
 const Mealitem = ({ data }) => {
-    console.log(data);
+    
     let navigate=useNavigate()
     return (
         <>
