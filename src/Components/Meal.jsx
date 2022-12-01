6265a7fb2d3318428f1351e1881e0b544d5d676f
Athul Thomas <athulthomas001798@gmail.com>
2022-11-30 21:41:28 -0800

    meal is set

--- src/Components/Meal.jsx
+++ src/Components/Meal.jsx
@@ -10,7 +10,7 @@ const Meal = () => {
   const[search,setSearch]=useState("");
   useEffect(()=>{
     fetch(url).then(res=>res.json()).then(data=>{
-     console.log(data.meals);
+     
       setItem(data.meals)
       setshow(true);
     })
